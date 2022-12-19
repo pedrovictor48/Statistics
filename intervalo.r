4df00da24b9c6a645ea3acfa5c3afed084bdae6f
Pedro Ferreira <64147890+pedrovictor48@users.noreply.github.com>
2022-12-18 21:25:51 -0300

    intervalo de confianca

---
+++ intervalo.r
@@ -0,0 +1,25 @@
+## caso variancia populacao e media 
+function(alfa, sigma, n, x_barra) {
+    z = qnorm(1 - alfa/2)
+    erro = z * sigma / sqrt(n)
+    L = x_barra - erro
+    R = x_barra + erro
+}
+
+## caso com a amostra
+function(x, conf_level) {
+    res = t.test(x, conf = conf_level)
+    L = res$conf.int[1]
+    R = res$conf.int[2]
+    print(L)
+    print(R)
+}
+
+## apenas com média e dp amostrais
+function(S, x_barra, alfa, n) {
+    t = abs(qt(alfa/2, df = n - 1))
+    
+    erro = t*S/sqrt(n)
+    L = x_barra - erro
+    R = x_barra + erro
+}
